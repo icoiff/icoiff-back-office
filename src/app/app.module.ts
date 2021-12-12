b6a53dc73da3f42fb6fd0e8308b7fa89ab7b19a8
henimezrani <henimezrani@gmail.com>
2021-12-12 16:34:24 +0100

    remove unused pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,22 +18,11 @@ import { TablesComponent } from "./views/admin/tables/tables.component";
 // auth views
 import { LoginComponent } from "./views/auth/login/login.component";
 
-// no layouts views
-import { LandingComponent } from "./views/landing/landing.component";
-
 // components for views and layouts
 
 import { AdminNavbarComponent } from "./components/navbars/admin-navbar/admin-navbar.component";
-import { AuthNavbarComponent } from "./components/navbars/auth-navbar/auth-navbar.component";
-import { FooterAdminComponent } from "./components/footers/footer-admin/footer-admin.component";
-import { FooterComponent } from "./components/footers/footer/footer.component";
-import { FooterSmallComponent } from "./components/footers/footer-small/footer-small.component";
 import { HeaderStatsComponent } from "./components/headers/header-stats/header-stats.component";
-import { IndexNavbarComponent } from "./components/navbars/index-navbar/index-navbar.component";
-import { IndexDropdownComponent } from "./components/dropdowns/index-dropdown/index-dropdown.component";
-import { TableDropdownComponent } from "./components/dropdowns/table-dropdown/table-dropdown.component";
 import { PagesDropdownComponent } from "./components/dropdowns/pages-dropdown/pages-dropdown.component";
-import { NotificationDropdownComponent } from "./components/dropdowns/notification-dropdown/notification-dropdown.component";
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { UserDropdownComponent } from "./components/dropdowns/user-dropdown/user-dropdown.component";
 import { ManageSalonsComponent } from "./views/admin/manage-salons/manage-salons.component";
@@ -53,26 +42,17 @@ import { Error404Component } from "./views/error404/error404.component";
   declarations: [
     AppComponent,
     DashboardComponent,
-    IndexDropdownComponent,
     PagesDropdownComponent,
-    TableDropdownComponent,
-    NotificationDropdownComponent,
     UserDropdownComponent,
     SidebarComponent,
-    FooterComponent,
-    FooterSmallComponent,
-    FooterAdminComponent,
     HeaderStatsComponent,
-    AuthNavbarComponent,
     AdminNavbarComponent,
-    IndexNavbarComponent,
     AdminComponent,
     AuthComponent,
     MapsComponent,
     SettingsComponent,
     TablesComponent,
     LoginComponent,
-    LandingComponent,
     ManageSalonsComponent,
     TableSalonsComponent,
     TableClientsComponent,
