5c8cedecc103f4c41aee82f289885cbe69ee8314
henimezrani <henimezrani@gmail.com>
2022-01-14 19:26:57 +0100

    mailer implemented

--- src/app/services/mailer-service.service.ts
+++ src/app/services/mailer-service.service.ts
@@ -1,16 +1,21 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class MailerServiceService {
   endpoint: string = environment.apiUrl;
-  constructor(private http: HttpClient) { }
-  sendMail(email: string) {
-    return this.http.post<any>(`${this.endpoint}/email`, {
-      email
-    })
+  constructor(private http: HttpClient) {}
+  sendRegistrationMail(email: string) {
+    return this.http.post<any>(`${this.endpoint}/email/register`, {
+      email,
+    });
+  }
+  sendResetPasswordMail(email: string) {
+    return this.http.post<any>(`${this.endpoint}/email/reset`, {
+      email,
+    });
   }
 }
